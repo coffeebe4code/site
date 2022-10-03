596c278f9b84f4cbfbda29215c0276d33ffb0995
Chris <chris.clark@coffeeb4code.com>
2022-10-03 14:39:36 -0500

    need to remove html modules

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,17 @@
+import {LitElement, html} from 'lit';
+
+class MyElement extends LitElement {
+
+  render(){
+    return html`
+      <div>
+        <p>A paragraph</p>
+      </div>
+    `;
+  }
+}
+
+customElements.define('my-element', MyElement);
+
 console.log('hello');
+
